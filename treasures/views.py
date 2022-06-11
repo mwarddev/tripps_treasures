d9cc06862c403b40959f5429134ceb1cbe866076
mwarddev <martinward1981@googlemail.com>
2022-06-11 22:11:30 +0000

    Add search functionality

--- treasures/views.py
+++ treasures/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Treasure, Category, Image
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
+from .models import Treasure, Category
 
 
 def all_treasures(request):
@@ -8,7 +10,7 @@ def all_treasures(request):
     # treasures = Treasure.objects.prefetch_related('images')
     treasures = Treasure.objects.all()
 
-    # query = None
+    query = None
     categories = None
 
     if request.GET:
@@ -18,10 +20,20 @@ def all_treasures(request):
             treasures = treasures.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
+        if 'search' in request.GET:
+            query = request.GET['search']
+            if not query:
+                # messages.error(request, 'No search criteria entered')
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            treasures = treasures.filter(queries)
+
     template_name = 'treasures/treasures.html'
     context = {
         'treasures': treasures,
         'selected_categories': categories,
+        'search_query': query,
     }
 
     return render(request, template_name, context)
